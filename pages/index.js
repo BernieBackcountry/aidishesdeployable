cb604d22ed9652cf1addf430122d965afc744bca
jasontills99 <70411008+JasonTilley21@users.noreply.github.com>
2023-01-25 15:45:39 -0700

    working product

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,16 @@
 import { useState } from "react";
 import styles from "./index.module.css";
-
-
+import generate from "./api/generate";
 
 
 export default function Home() {
   const [foodInput, setfoodInput] = useState("");
-  const [result, setResult] = useState();
+  const [titleWords, setTitleWords] = useState();
   const [counter, setCounter] = useState(1);
+  const [ingredientWords, setIngredientWords] = useState();
+  const [instructionWords, setInstructionWords] = useState();
+  const [imageUrl, setImageUrl] = useState();
   const [ingredients, setIngredients] = useState([{ id: 1, value: "" }]);
-  //const [image, setImage] = useState();
-
-
 
   const onSubmit = async (event) => {
     event.preventDefault();
@@ -28,36 +27,16 @@ export default function Home() {
       if (response.status !== 200) {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
-      setResult(data.result);
+      setTitleWords(data.titleWords);
+      setIngredientWords(data.ingredientWords)
+      setInstructionWords(data.instructionWords)
+      setImageUrl(data.imageUrl);
       setfoodInput("");
 
     } catch (error) {
       console.error(error);
       alert(error.message);
     }
-
-    /*try {
-      const response = await fetch("/api/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ food: foodInput }),
-      });
-
-      const data = await response.json();
-      if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
-      }
-      setResult(data.result);
-      setfoodInput("");
-
-    } catch (error) {
-      console.error(error);
-      alert(error.message);
-    }
-
-*/
   }
 
   const onChange = (e, id) => {
@@ -66,45 +45,50 @@ export default function Home() {
     newIngredients[index].value = e.target.value;
     setIngredients(newIngredients);
     setfoodInput(newIngredients.map((i) => i.value).join(","));
-};
+  };
 
   const addRow = () => {
     setIngredients([...ingredients, { id: Date.now(), value: "" }]);
   }
 
- 
-
 
   return (
     <div>
-       <form id="ingredientsForm" onSubmit={onSubmit} className={styles.ingredientform}>
+      <form id="ingredientsForm" onSubmit={onSubmit} className={styles.ingredientform}>
         <div className={styles.format}>
-            {ingredients
-                .filter((ingredient) => ingredient.value != null)
-                .map((ingredient) => (
-                    <div key={ingredient.id}>
-                        <input
-                            type="text"
-                            placeholder="Enter an Ingredient"
-                            defaultValue={ingredient.value}
-                            onChange={(e) => onChange(e, ingredient.id)}
-                            className={styles.ingredientform}
-                        />
-                    </div>
-                ))}
+          {ingredients
+            .filter((ingredient) => ingredient.value != null)
+            .map((ingredient) => (
+              <div key={ingredient.id}>
+                <input
+                  type="text"
+                  placeholder="Enter an Ingredient"
+                  defaultValue={ingredient.value}
+                  onChange={(e) => onChange(e, ingredient.id)}
+                  className={styles.ingredientform}
+                />
+              </div>
+            ))}
         </div>
         <div>
-            <button type="button" onClick={addRow} className={styles.generatebtn}>
-                ADD
-            </button>
-            <br />
-            <input type="submit" value="Generate Recipes" className={styles.generatebtn} />
+          <button type="button" onClick={addRow} className={styles.generatebtn}>
+            ADD
+          </button>
+          <br />
+          <input type="submit" value="Generate Recipes" className={styles.generatebtn} />
         </div>
-    </form>
-      <div id="final" className={styles.result}></div>
-      {
-        <div id="final" dangerouslySetInnerHTML={{ __html: result }} className={styles.result} />
-      }
+      </form>
+      <div id="title" className={styles.title}>{titleWords}</div>
+      <br/>
+      <br/>
+      <div id="ingredients" className={styles.ingredients}>{ingredientWords}</div>
+      <br/>
+      <br/>
+      <div id="instructions" className={styles.instructions}>{instructionWords}</div>
+      <br/>
+      <div>
+        <img src={imageUrl} alt="Recipe Image" className={styles.image} />
+      </div>
     </div>
   );
 }
