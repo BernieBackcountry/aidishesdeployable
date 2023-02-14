ef1ed2a51b6826cff8c071a4210da0fc40bdd8f5
jasontills99 <70411008+JasonTilley21@users.noreply.github.com>
2023-02-14 08:22:39 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
       setInstructionWords(data.instructionWords)
       setNutritionWords(data.nutritionWords)
       setImageUrl(data.imageUrl);
-      
+
 
     } catch (error) {
       console.error(error);
@@ -65,10 +65,13 @@ export default function Home() {
       .join(","));
   };
 
+
+
+
   return (
     <html className={styles.page}>
       <head><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1482013293005685"
-     crossorigin="anonymous"></script></head>
+        crossorigin="anonymous"></script></head>
       <div >
         <form id="ingredientsForm" onSubmit={onSubmit} className={styles.ingredientform}>
           <div className={styles.format}>
@@ -96,34 +99,43 @@ export default function Home() {
             <br />
             <input type="submit" value="Generate Recipes" className={styles.generatebtn} />
           </div>
+          <br />
+          <div>
+            <a target="_blank"
+              className={styles.generatebtn}
+              href="https://www.amazon.com/alm/storefront?almBrandId=QW1hem9uIEZyZXNo&amp;tag=googhydr-20&amp;hvadid=439740365166&amp;hvpos=&amp;hvexid=&amp;hvnetw=g&amp;hvrand=3841882910299123143&amp;hvpone=&amp;hvptwo=&amp;hvqmt=e&amp;hvdev=c&amp;hvdvcmdl=&amp;hvlocint=&amp;hvlocphy=9029027&amp;hvtargid=kwd-11554712357&amp;ref=pd_sl_7cf2rld4ut_e&_encoding=UTF8&tag=aidishes-20&linkCode=ur2&linkId=8da7828e5eb0765ec90e642fc07d6938&camp=1789&creative=9325">
+              ORDER MISSING INGREDIENTS ON AMAZON</a>
+          </div>
         </form>
         <div> {
           isLoading ? <img src="https://media.tenor.com/PB91j4e7Q6oAAAAj/pizza-food.gif" className={styles.loading} /> :
-              <div classname={styles.container} style={showBorder ? { border: '20px solid #D98880' } : {}}>
-                <div id="title" className={styles.title}>{titleWords}</div>
-                <br />
-                <br />
+            <div classname={styles.container} style={showBorder ? { border: '20px solid #D98880' } : {}}>
+              <div id="title" className={styles.title}>{titleWords}</div>
+              <br />
+              <br />
 
-                <div id="ingredients" className={styles.ingredients}>{ingredientWords}</div>
-                <br />
-                <br />
+              <div id="ingredients" className={styles.ingredients}>{ingredientWords}</div>
+              <br />
+              <br />
 
-                <div id="instructions" className={styles.instructions}>{instructionWords}</div>
+              <div id="instructions" className={styles.instructions}>{instructionWords}</div>
 
-                <br />
-                <br />
+              <br />
+              <br />
 
-                <div id="nutrition" className={styles.nutrition}>{nutritionWords}</div>
+              <div id="nutrition" className={styles.nutrition}>{nutritionWords}</div>
 
-                <br />
-                <br />
+              <br />
+              <br />
 
-                {showImage ? <img src={imageUrl} alt="Recipe Image" className={styles.image} /> : null}
+              {showImage ? <img src={imageUrl} alt="Recipe Image" className={styles.image} /> : null}
 
-              </div>
-      }
             </div>
-    </div>
+        }
+        </div>
+      </div>
+
+
     </html>
   );
 }
